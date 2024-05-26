14aa530319dad96884d43b32adbf1c18bd6fc3f0
nabielsubhan <mnsubhan1509@gmail.com>
2024-05-26 20:57:11 +0700

    fix sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.sonarqube") version "4.4.1.3373"
     jacoco
 }
 
@@ -10,6 +11,14 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "KoleksiKota_backend-subscription")
+        property("sonar.organization", "koleksikota")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
 
 configurations {
